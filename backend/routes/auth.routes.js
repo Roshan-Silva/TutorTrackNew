154bdc0e15d30df3bc399ab9e9194ac8667c19de
Roshan-Silva <roshan0122silva@gmail.com>
2024-06-24 13:17:07 +0530

    2-Add role for the users

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { getMe, signup, login, logout } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/protectRoute.js';
+import { protectRole } from '../middleware/protectRole.js';
+
+const router = express.Router();
+
+router.get('/me', protectRoute, getMe);
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/logout', logout);
+
+router.get('/teacher-data', protectRoute, protectRole(['teacher']), (req, res) => {
+    res.status(200).json({ message: "Teacher data" });
+});
+
+router.get('/student-data', protectRoute, protectRole(['student']), (req, res) => {
+    res.status(200).json({ message: "Student data" });
+});
+
+export default router;
